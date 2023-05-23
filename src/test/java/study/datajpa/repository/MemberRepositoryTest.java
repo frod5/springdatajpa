34cf2bb04f81fdeb95c18a34835c602509f109d0
sunwooKim <frod5@kakao.com>
2023-05-23 14:37:26 +0900

    실전! Spring Data Jpa - 나머지기능들 - Native 쿼리

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -415,4 +415,52 @@ class MemberRepositoryTest {
         //실무의 복잡한 쿼리를 해결하기에는 한계가 있다.
         //실무에서는 단순할 때만 사용하고, 조금만 복잡해지면 QueryDSL을 사용하자
     }
+
+    @Test
+    void nativeQuery() {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member member1 = new Member("m1", 0, teamA);
+        Member member2 = new Member("m2", 0, teamA);
+        em.persist(member1);
+        em.persist(member2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        Member result = memberRepository.findByNativeQuery("m1");
+        System.out.println("result = " + result);
+
+
+        //Native Query + Projections
+        Page<MemberProjection> result1 = memberRepository.findByNativeProjection(PageRequest.of(1, 10));
+        List<MemberProjection> content = result1.getContent();
+
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection.username = " + memberProjection.getUsername());
+            System.out.println("memberProjection.teamName = " + memberProjection.getTeamName());
+        }
+
+        //네이티브 쿼리
+        //가급적 네이티브 쿼리는 사용하지 않는게 좋음, 정말 어쩔 수 없을 때 사용
+        //최근에 나온 궁극의 방법 스프링 데이터 Projections 활용
+
+        //스프링 데이터 JPA 기반 네이티브 쿼리
+        //페이징 지원
+        //반환 타입
+            //Object[]
+            //Tuple
+            //DTO(스프링 데이터 인터페이스 Projections 지원)
+        //제약
+        //Sort 파라미터를 통한 정렬이 정상 동작하지 않을 수 있음(믿지 말고 직접 처리)
+        //JPQL처럼 애플리케이션 로딩 시점에 문법 확인 불가
+        //동적 쿼리 불가
+
+        //동적 네이티브 쿼리
+        //하이버네이트를 직접 활용
+        //스프링 JdbcTemplate, myBatis, jooq같은 외부 라이브러리 사용
+    }
 }
